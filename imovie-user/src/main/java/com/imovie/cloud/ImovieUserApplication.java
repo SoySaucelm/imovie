52a004eef14ee9abc8d37f06bb2d09912089a2eb
soysauce <soysauce_lm@163.com>
2019-02-25 18:02:21 +0800

    add code

--- imovie-user/src/main/java/com/imovie/cloud/ImovieUserApplication.java
+++ imovie-user/src/main/java/com/imovie/cloud/ImovieUserApplication.java
@@ -2,8 +2,10 @@ package com.imovie.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @SuppressWarnings(value={"all"})
 public class ImovieUserApplication {
 
