03a539b4afb9e653170d67833a25b4bb40b23c55
soysauce <soysauce_lm@163.com>
2019-02-26 17:39:55 +0800

    code

--- consumer-movie/src/test/java/com/imovie/cloud/ConsumerMovieApplicationTests.java
+++ consumer-movie/src/test/java/com/imovie/cloud/ConsumerMovieApplicationTests.java
@@ -3,7 +3,18 @@ package com.imovie.cloud;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.support.BasicAuthorizationInterceptor;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.InputStream;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -12,5 +23,6 @@ public class ConsumerMovieApplicationTests {
     @Test
     public void contextLoads() {
     }
+
 
 }
